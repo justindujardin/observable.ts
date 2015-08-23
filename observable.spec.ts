a86b925204040c30e256eba9c18cfc6c9691cb59
Justin DuJardin <justin@dujardinconsulting.com>
2015-08-23 13:48:12 -0700

    feat: functional observable implementation with test suite
     - works for basic assertions of spec, still incomplete.
     - tests 100% of code with suite assertions.

--- observable.spec.ts
+++ observable.spec.ts
@@ -1,27 +1,34 @@
 /// <reference path="typings/jasmine/jasmine.d.ts"/>
 /// <reference path="observable.ts"/>
 
-describe('observable', () => {
+describe('obs', () => {
+
   describe('IObservable', () => {
 
     describe('subscribe', () => {
 
       it('should add listener to list', () => {
-        var o:any = new observable.Observable();
+        var o:any = new obs.Observable();
         expect(o._listeners.length).toBe(0);
         o.subscribe({});
         expect(o._listeners.length).toBe(1);
       });
 
       it('should return a subscription with an unsubscribe method', () => {
-        var o:any = new observable.Observable();
-        var s:observable.ISubscription = o.subscribe({});
+        var o:any = new obs.Observable();
+        var s:obs.ISubscription = o.subscribe({});
         expect(s.unsubscribe).toBeDefined();
       });
 
-      it('should return null if require generator argument is missing', () => {
-        var o:any = new observable.Observable();
+      it('should return null if required generator argument is missing', () => {
+        var o:any = new obs.Observable();
         expect(o.subscribe(null)).toBeNull();
+      });
+
+      it('should return null if required generator argument is not an object', () => {
+        var o:any = new obs.Observable();
+        expect(o.subscribe([])).toBeNull();
+        expect(o.subscribe("notanobject")).toBeNull();
       });
 
     });
@@ -30,11 +37,11 @@ describe('observable', () => {
 
     describe('unsubscribe', () => {
 
-      it('should remove a listener from the observable when unsubscribe is called', () => {
-        var o:any = new observable.Observable();
-        var generator:observable.IGenerator = {};
+      it('should remove a listener from the obs when unsubscribe is called', () => {
+        var o:any = new obs.Observable();
+        var generator:obs.IGenerator = {};
         o.subscribe({});
-        var s:observable.ISubscription = o.subscribe(generator);
+        var s:obs.ISubscription = o.subscribe(generator);
         expect(o._listeners.length).toBe(2);
         expect(s.unsubscribe).toBeDefined();
         s.unsubscribe();
@@ -42,12 +49,93 @@ describe('observable', () => {
       });
 
       it('should have no side effects when called multiple times', () => {
-        var o:any = new observable.Observable();
-        var s:observable.ISubscription = o.subscribe({});
+        var o:any = new obs.Observable();
+        var s:obs.ISubscription = o.subscribe({});
         s.unsubscribe();
         s.unsubscribe();
       });
 
+    });
+  });
+
+  describe('IGenerator', () => {
+    function expectNotCalledAfterUnsubscribe(operation:string) {
+      var count:number = 0;
+      var o:any = new obs.Observable();
+      var generator:any = {};
+      generator[operation] = (value?:any):any => count++;
+      var subscription:obs.ISubscription = o.subscribe(generator);
+      o[operation]();
+      expect(count).toBe(1);
+      subscription.unsubscribe();
+      o[operation]();
+      expect(count).toBe(1);
+    }
+
+    function expectUnsubscribeOnDoneTrue(operation:string) {
+      var o:any = new obs.Observable();
+      var generator:any = {};
+      generator[operation] = (value?:any):any => {
+        return {
+          done: true
+        };
+      };
+      o.subscribe(generator);
+      expect(o._listeners.length).toBe(1);
+      o[operation]();
+      expect(o._listeners.length).toBe(0);
+    }
+
+    function expectThrowOnCallbackThrow(operation:string) {
+      var called:boolean = false;
+      var o:any = new obs.Observable();
+      var e = new Error('Test Error');
+      var generator:any = {
+        throw: (error:string) => {
+          expect(error).toBe(e);
+          called = true;
+        }
+      };
+      generator[operation] = (value?:any):any => {
+        throw e;
+      };
+      o.subscribe(generator);
+      o[operation]();
+      expect(called).toBe(operation !== 'throw');
+    }
+
+    describe('next', () => {
+      it('must never be called after the unsubscribe method of subscription has been called', () => {
+        expectNotCalledAfterUnsubscribe('next');
+      });
+      it('should unsubscribe if an object {done:true} is returned from handler', () => {
+        expectUnsubscribeOnDoneTrue('next');
+      });
+      it('errors thrown during execution are reported to a generator with throw method defined', () => {
+        expectThrowOnCallbackThrow('next');
+      });
+    });
+    describe('throw', () => {
+      it('must never be called after the unsubscribe method of subscription has been called', () => {
+        expectNotCalledAfterUnsubscribe('throw');
+      });
+      it('should unsubscribe if an object {done:true} is returned from handler', () => {
+        expectUnsubscribeOnDoneTrue('throw');
+      });
+      it('errors thrown during execution are reported to a generator with throw method defined', () => {
+        expectThrowOnCallbackThrow('throw');
+      });
+    });
+    describe('return', () => {
+      it('must never be called after the unsubscribe method of subscription has been called', () => {
+        expectNotCalledAfterUnsubscribe('return');
+      });
+      it('should unsubscribe if an object {done:true} is returned from handler', () => {
+        expectUnsubscribeOnDoneTrue('return');
+      });
+      it('errors thrown during execution are reported to a generator with throw method defined', () => {
+        expectThrowOnCallbackThrow('return');
+      });
     });
   });
 });
--- observable.ts
+++ observable.ts
@@ -1,25 +1,62 @@
-module observable {
+module obs {
 
   /**
-   * An object that generates observable events.
+   * Determine if a given parameter is an object or not.
+   * @returns {boolean} true if the type is an object.
+   */
+  export function isObject(obj:any):boolean {
+    var type = typeof obj;
+    return type === 'object' && !!obj && toString.call(obj) !== '[object Array]';
+  }
+
+  /**
+   * An object that generates observable values.
    */
   export interface IGenerator {
     /**
-     * Trigger an event for listeners to consume.
+     * Trigger/Receive a value.
      * @param value
      */
     next?(value?:any):void;
 
-    throw?(error: any):void;
+    /**
+     * Trigger/Receive an error.
+     * @param error The error that occurred during execution.
+     */
+    throw?(error:any):void;
 
+    /**
+     * Trigger/Receive a final value.
+     * @param value An optional value
+     */
     return?(value?:any):void;
   }
 
+  /**
+   * An object that is observable.  It generates values that
+   * interested parties may consume.  It declares a method to
+   * allow one to `subscribe` to those events.
+   */
   export interface IObservable {
+
+    /**
+     * Subscribe to changes in value for this object.
+     * @param {@link IGenerator} generator An object that defines the callbacks that
+     *        are to be consumed when a value changes.
+     */
     subscribe(generator:IGenerator):ISubscription;
   }
 
+  /**
+   * An object that manages the lifetime of a generator that is associated with
+   * an {@link IObservable}.
+   */
   export interface ISubscription {
+    /**
+     * Unsubscribe the {@link IGenerator} associated with this subscription.  The
+     * associated generator will immediately stop receiving values.  Calling this
+     * method multiple times will have no side-effects.
+     */
     unsubscribe():void;
   }
 
@@ -27,8 +64,8 @@ module observable {
    * Implementation of an Observable object that conforms to the observable specification.
    */
   export class Observable implements IObservable, IGenerator {
-    subscribe(generator:observable.IGenerator):observable.ISubscription {
-      if (!generator) {
+    subscribe(generator:obs.IGenerator):obs.ISubscription {
+      if (!generator || !isObject(generator)) {
         // TODO: The spec is unclear about what to do if the required argument is missing.
         return null;
       }
@@ -44,16 +81,52 @@ module observable {
       };
     }
 
+    next(value?:any):void {
+      return this._execute('next', value);
+    }
+
+    throw(error:any):void {
+      return this._execute('throw', error);
+    }
+
+    return(value?:any):void {
+      return this._execute('return', value);
+    }
 
 
     private _listeners:IGenerator[] = [];
+
+    private _execute(operation:string, value?:any):void {
+      var completed:obs.IGenerator[] = [];
+      for (var i:number = 0; i < this._listeners.length; i++) {
+        try {
+          var generator:any = this._listeners[i];
+          var result:any = generator[operation](value);
+          if (result && result.done === true) {
+            completed.push(this._listeners[i]);
+          }
+        }
+        catch (e) {
+          if (operation === 'throw') {
+            console.error("Error thrown in error handler: ", e);
+          }
+          else {
+            this.throw(e);
+          }
+          break;
+        }
+      }
+      if (completed.length) {
+        completed.forEach((g:obs.IGenerator) => this._removeGenerator(g));
+      }
+    }
 
     /**
      * Remove a generator by its instance.
      * @param generator The generator to remove.
      * @private
      */
-    private _removeGenerator(generator:observable.IGenerator) {
+    private _removeGenerator(generator:obs.IGenerator) {
       for (var i:number = 0; i < this._listeners.length; i++) {
         if (this._listeners[i] === generator) {
           this._listeners.splice(i, 1);
