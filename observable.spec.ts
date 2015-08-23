ae37bfea4e9efab010e158cd3ffd0b8396af0b32
Justin DuJardin <justin@dujardinconsulting.com>
2015-08-22 17:06:11 -0700

    chore: expand spec and define generator interface
     - make observable object implement IGenerator, so the user invokes the generator interface, and the given `generator` for a subscription receives notification by invoking the same functions on it.
     - the spec avoids talking about how an observable is constructed, but this API with re-use between generator and subscriber seems to make sense.  (opinionated)

--- observable.spec.ts
+++ observable.spec.ts
@@ -2,30 +2,34 @@
 /// <reference path="observable.ts"/>
 
 describe('observable', () => {
-  it('should be defined', () => {
-    expect(observable.Observable).toBeDefined();
-  });
   describe('IObservable', () => {
+
     describe('subscribe', () => {
+
       it('should add listener to list', () => {
         var o:any = new observable.Observable();
         expect(o._listeners.length).toBe(0);
         o.subscribe({});
         expect(o._listeners.length).toBe(1);
       });
+
       it('should return a subscription with an unsubscribe method', () => {
         var o:any = new observable.Observable();
         var s:observable.ISubscription = o.subscribe({});
         expect(s.unsubscribe).toBeDefined();
       });
+
       it('should return null if require generator argument is missing', () => {
         var o:any = new observable.Observable();
         expect(o.subscribe(null)).toBeNull();
       });
+
     });
   });
   describe('ISubscription', () => {
+
     describe('unsubscribe', () => {
+
       it('should remove a listener from the observable when unsubscribe is called', () => {
         var o:any = new observable.Observable();
         var generator:observable.IGenerator = {};
@@ -36,6 +40,14 @@ describe('observable', () => {
         s.unsubscribe();
         expect(o._listeners.length).toBe(1);
       });
+
+      it('should have no side effects when called multiple times', () => {
+        var o:any = new observable.Observable();
+        var s:observable.ISubscription = o.subscribe({});
+        s.unsubscribe();
+        s.unsubscribe();
+      });
+
     });
   });
 });
--- observable.ts
+++ observable.ts
@@ -1,11 +1,18 @@
 module observable {
 
+  /**
+   * An object that generates observable events.
+   */
   export interface IGenerator {
     /**
      * Trigger an event for listeners to consume.
      * @param value
      */
-    next?(value?:any):any;
+    next?(value?:any):void;
+
+    throw?(error: any):void;
+
+    return?(value?:any):void;
   }
 
   export interface IObservable {
@@ -19,21 +26,34 @@ module observable {
   /**
    * Implementation of an Observable object that conforms to the observable specification.
    */
-  export class Observable implements IObservable {
+  export class Observable implements IObservable, IGenerator {
     subscribe(generator:observable.IGenerator):observable.ISubscription {
       if (!generator) {
         // TODO: The spec is unclear about what to do if the required argument is missing.
         return null;
       }
+      var expired:boolean = false;
       this._listeners.push(generator);
       return {
-        unsubscribe: this._unsubscribe.bind(this, generator),
-      }
+        unsubscribe: () => {
+          if (!expired) {
+            expired = true;
+            this._removeGenerator(generator);
+          }
+        }
+      };
     }
+
+
 
     private _listeners:IGenerator[] = [];
 
-    private _unsubscribe(generator:observable.IGenerator):void {
+    /**
+     * Remove a generator by its instance.
+     * @param generator The generator to remove.
+     * @private
+     */
+    private _removeGenerator(generator:observable.IGenerator) {
       for (var i:number = 0; i < this._listeners.length; i++) {
         if (this._listeners[i] === generator) {
           this._listeners.splice(i, 1);
